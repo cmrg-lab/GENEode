bec94c3e23674cfb0e1c5b21aa213525803abb63
CMRG Lab <cmrglab@gmail.com>
2020-02-03 14:19:38 -0800

    Create geneode.py

---
+++ geneode/geneode.py
@@ -0,0 +1,111 @@
+###------Network Simulator------###
+###------Shulin Cao------###
+###------CMRG, UC San Diego------###
+
+###import packages###
+import pandas as pd
+import collections
+import timeit
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import networkx as nx
+import matplotlib.mlab as mlab
+import statistics
+import random
+import numpy.linalg
+import numpy as np
+import sys
+from scipy.optimize import minimize
+
+from sklearn.linear_model import LinearRegression
+from sklearn import cluster
+import seaborn as sns
+sns.set()
+from sklearn import datasets
+from sklearn.metrics import r2_score
+from matplotlib import pylab
+from matplotlib import font_manager
+import chaospy as cp 
+import uncertainpy as un
+
+def get_reactors(reac):
+    reac_split = reac.split(' ')
+    reactors = []
+    for k in reac_split:
+        if k != '&' and k!= '=>':
+            reactors.append(k)
+    return reactors[:-1]
+
+
+def Hill(reactor, n, EC50):
+    B = (EC50**n-1)/(2*EC50**n-1)
+    C = (B-1)**(1/n)
+    if reactor[0] == '!':
+        return (1-B*globals()['{}'.format(reactor[1:])]**n/(C**n + globals()['{}'.format(reactor[1:])]**n))
+    else:
+        return B*globals()['{}'.format(reactor)]**n/(C**n + globals()['{}'.format(reactor)]**n)
+
+
+def OR(reaction_list):
+    tera = (-1)**(len(reaction_list)+1)
+    for k in reaction_list:
+        weight, n, EC50 = reaction_list[k]
+        final = weight
+        for j in get_reactors(k):
+            final *= Hill(j, n, EC50)
+        tera *= (final-1)
+    tera +=1
+    return tera
+    
+def inte(state, t, reaction_dict):
+    for i in range(len(node_ID)):
+        globals()['{}'.format(node_ID[i])] = state[i]
+    for i in range(len(node_ID)):
+        if len(reaction_dict[node_ID[i]]) == 1:
+            reactors = get_reactors(list(reaction_dict[node_ID[i]].keys())[0])
+            weight, n, EC50 = reaction_dict[node_ID[i]][list(reaction_dict[node_ID[i]].keys())[0]]
+            TF = 1
+            for j in reactors:
+                TF *= Hill(j, n, EC50)
+            globals()['{}'.format(node_ID[i] + 'd')] = (TF*weight*Ymax[i]-globals()['{}'.format(node_ID[i])])/tau[i]
+        else:
+            TF = OR(reaction_dict[node_ID[i]])
+            globals()['{}'.format(node_ID[i] + 'd')] = (TF*Ymax[i]-globals()['{}'.format(node_ID[i])])/tau[i]
+    return [globals()['{}'.format(k+ 'd')] for k in node_ID]
+    
+reactions_raw = pd.read_excel('/home/shulincao/Desktop/projects/demo/MTv29-philip-20170921-for-network.xlsx', sheet_name = 1, skiprows = 1, header = 0)
+species_raw = pd.read_excel('/home/shulincao/Desktop/projects/demo/MTv29-philip-20170921-for-network.xlsx', sheet_name = 0, skiprows = 1, header = 0)
+pmid = reactions_raw['PMID'].tolist()
+
+species = species_raw[['ID', 'Yinit', 'Ymax', 'tau']]
+node_ID = species['ID'].tolist()
+Yinit = species['Yinit'].tolist()
+Ymax = species['Ymax'].tolist()
+tau = species['tau'].tolist()
+
+species_dict = dict()
+for k in range(len(species)):
+    #lis = species.loc[k, ['Yinit', 'Ymax', 'tau']].tolist()
+    species_dict[species.loc[k, 'ID']] = species.loc[k, ['Yinit', 'Ymax', 'tau']].tolist()
+
+state0 = []
+for k in range(len(node_ID)):
+    state0.append(Yinit[k])  #solve_ivp
+
+t = np.arange(0.0, 60.0*4, 0.1)
+
+reactions = reactions_raw[['rule', 'weight', 'n', 'EC50']]
+reaction_dict = collections.defaultdict(dict)
+original_reaction_dict = collections.defaultdict(dict)
+for k in range(len(reactions)):
+    node = reactions.loc[k, 'rule'].split(' ')
+    reaction_dict[node[-1]][reactions.loc[k, 'rule']] = reactions.loc[k, ['weight', 'n', 'EC50']].tolist()
+    original_reaction_dict[node[-1]][reactions.loc[k, 'rule']] = reactions.loc[k, ['weight', 'n', 'EC50']].tolist()
+
+inpu_HI = 0.7
+yHill_ss = odeint(inte, state0, t, args = (inpu_HI, reaction_dict))
+
+
+
+
+
